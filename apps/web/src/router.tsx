a8cc6157ebe8ca66e83725fdbc6c9f52d65066de
Muhammad Zahid Masruri <masruri03@gmail.com>
2025-08-12 09:52:56 +0700

    Rename app to web and update Tanstack Query import

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -1,14 +1,14 @@
 import type { I18n } from '@lingui/core';
 import { createRouter as createTanstackRouter } from '@tanstack/react-router';
 import { setupRouterSsrQueryIntegration } from '@tanstack/react-router-ssr-query';
-import * as TanstackQuery from './integrations/tanstack-query/root-provider';
+import { getContext } from './integrations/tanstack-query/root-provider';
 import { routerWithLingui } from './lib/lingui/i18n-router-plugin';
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
 
 // Create a new router instance
 export const createRouter = ({ i18n }: { i18n: I18n }) => {
-  const rqContext = TanstackQuery.getContext();
+  const rqContext = getContext();
 
   const router = routerWithLingui(
     createTanstackRouter({
